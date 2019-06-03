5eae811002375a8883ccf1fb029d4eff86ba07b7
Peter Basem <pero.bassem@gmail.com>
2019-06-03 11:39:55 +0200

    Initial Commit

---
+++ TableViewWithDifferentRows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewWithDifferentRows
+//
+//  Created by Peter Bassem on 6/3/19.
+//  Copyright © 2019 Peter Bassem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
